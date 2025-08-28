c147c7f73913d57e46d0d25c403dbc674fe1a492
eanavi <eanavi@gmail.com>
2025-08-28 15:53:42 +0200

    Correccion de pruebas

---
+++ Documentacion/DesplegarEnIIS.ps1
@@ -0,0 +1,28 @@
+﻿Write-Host "============= Inicio de la Publicacion +============"
+$rutaProyecto = "C:\inetpub\wwwroot\AnhApi"
+$rutaPublicacion = "$rutaProyecto\publish"
+Write-Host "=============== Deteniendo el servidor IIS ============="
+iisreset /stop
+Write-Host "========== Limpiando la carpeta de publicacion ========="
+cd $rutaProyecto
+if(Test-Path $rutaPublicacion){
+    Remove-Item -Recurse -Force $rutaPublicacion   
+}
+
+dotnet clean
+dotnet build -c Release
+
+Write-Host "=================== Publicando ================"
+dotnet publish -c Release -r win-x64 --self-contained  true -o $rutaPublicacion
+Write-Host "============== creando archivos ==============="
+cd $rutaPublicacion
+New-Item -Path "$rutaPublicacion\.env" -ItemType File -Value "ENTORNO_ASPNETCORE=Publication"
+$rutaLogs = "$rutaPublicacion\logs"
+if(-Not (Test-Path $rutaLogs)){
+    New-Item -Path $rutaLogs -ItemType Directory | Out-Null
+}
+
+icacls $rutaLogs /grant "IIS_IUSRS":(OI)(CI)F
+
+Write-Host "====================== Reiniciar servicios de internet ============"
+iisreset /estart
--- Esquemas/LoginRequest.cs
+++ Esquemas/LoginRequest.cs
@@ -2,10 +2,10 @@
 
 public class LoginRequest
 {
-    [Required(ErrorMessage = "El campo 'Usuario' es obligatorio.")]
-    [StringLength(50, ErrorMessage = "El campo 'Usuario' no puede exceder los 50 caracteres.")]
+    [Required(ErrorMessage = "El campo 'login' es obligatorio.")]
+    [StringLength(50, ErrorMessage = "El campo 'login' no puede exceder los 50 caracteres.")]
     public string Login { get; set; }
-    [Required(ErrorMessage = "El campo 'Contraseña' es obligatorio.")]
-    [StringLength(100, ErrorMessage = "El campo 'Contraseña' no puede exceder los 100 caracteres.")]
+    [Required(ErrorMessage = "El campo 'clave' es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El campo 'clave' no puede exceder los 100 caracteres.")]
     public string Clave { get; set; }
 }
